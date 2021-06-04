9e195ea9c890712d0b3a3607c2a98f11a275ca76
mikaelle1992 <mikaelle.rubiaps02@gmail.com>
2021-06-04 20:36:06 -0300

    Update Category

--- backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -9,13 +9,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import com.devsuperior.dscatalog.services.exceptions.EntityNotFoundException;
+import com.devsuperior.dscatalog.services.exceptions.ResouceNotFoundException;
 
 @ControllerAdvice //permite que a classe intercept alguma excetion que acontecer na camada de resources e tratar aqui
 public class ResourceExceptionHandler {
 		
-		@ExceptionHandler(EntityNotFoundException.class)// sempre que acontecer alguma exception do tipo EntityNotFoundException(class) o tratamento sera direcionado para esse metodo 
-		 public ResponseEntity<StandardError> entityNotFound(EntityNotFoundException e, HttpServletRequest request){
+		@ExceptionHandler(ResouceNotFoundException.class)// sempre que acontecer alguma exception do tipo EntityNotFoundException(class) o tratamento sera direcionado para esse metodo 
+		 public ResponseEntity<StandardError> entityNotFound(ResouceNotFoundException e, HttpServletRequest request){
 			 StandardError err = new StandardError();
 			 err.setTimestamp(Instant.now());// now: metodo que pego o horario atual
 			 err.setStatus(HttpStatus.NOT_FOUND.value());//.value : converter para int
--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -12,7 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
-import com.devsuperior.dscatalog.services.exceptions.EntityNotFoundException;
+import com.devsuperior.dscatalog.services.exceptions.ResouceNotFoundException;
 
 @Service //ela registra a class como um componente que vai participar do sistema de injeção de dependencias automatizados do spring
 public class CategoryService {
@@ -30,7 +30,7 @@ public class CategoryService {
 	@Transactional(readOnly = true)
 	public CategoryDTO finById(Long id) {
 		Optional<Category> obj = repository.findById(id);
-		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+		Category entity = obj.orElseThrow(() -> new ResouceNotFoundException("Entity not found"));
 		//.orElseThrow permite definir uma chamade de exception
 		
 		return new CategoryDTO(entity);
@@ -40,9 +40,24 @@ public class CategoryService {
 	public CategoryDTO insert(CategoryDTO dto) {
 		Category entity = new Category();
 		entity.setName(dto.getName());
-		repository.save(entity);
+		entity= repository.save(entity);
 		
 		return new CategoryDTO(entity);
+	}
+
+	@Transactional
+	public CategoryDTO update(Long id,CategoryDTO dto ) {
+		try {
+		Category entity = repository.findById(id).get();
+		entity.setName(dto.getName());
+		entity= repository.save(entity);
+		return new CategoryDTO(entity);
+		
+		}
+		catch(ResouceNotFoundException e){
+			throw new ResouceNotFoundException("Id not found"+ id);
+		}
+		
 	}
 	
 	
