ccd2f9acb4fa6c59026df014e313edb1bcd53a6e
mikaelle1992 <mikaelle.rubiaps02@gmail.com>
2021-06-05 10:36:26 -0300

    Delete

--- backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
 import com.devsuperior.dscatalog.services.exceptions.ResouceNotFoundException;
 
 @ControllerAdvice //permite que a classe intercept alguma excetion que acontecer na camada de resources e tratar aqui
@@ -16,13 +17,28 @@ public class ResourceExceptionHandler {
 		
 		@ExceptionHandler(ResouceNotFoundException.class)// sempre que acontecer alguma exception do tipo EntityNotFoundException(class) o tratamento sera direcionado para esse metodo 
 		 public ResponseEntity<StandardError> entityNotFound(ResouceNotFoundException e, HttpServletRequest request){
+			HttpStatus status = HttpStatus.NOT_FOUND;
 			 StandardError err = new StandardError();
 			 err.setTimestamp(Instant.now());// now: metodo que pego o horario atual
-			 err.setStatus(HttpStatus.NOT_FOUND.value());//.value : converter para int
+			 err.setStatus(status.value());//.value : converter para int
 			 err.setError("Resource not found");
 			 err.setMessage(e.getMessage());
 			 err.setPath(request.getRequestURI());
 			 
-			 return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
-}
+			 return ResponseEntity.status(status).body(err);
+     }
+		
+		@ExceptionHandler(DatabaseException.class)// sempre que acontecer alguma exception do tipo EntityNotFoundException(class) o tratamento sera direcionado para esse metodo 
+		 public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request){
+			HttpStatus status = HttpStatus.BAD_REQUEST;
+			StandardError err = new StandardError();
+			 err.setTimestamp(Instant.now());// now: metodo que pego o horario atual
+			 err.setStatus(status.value());//.value : converter para int
+			 err.setError("Database exception");
+			 err.setMessage(e.getMessage());
+			 err.setPath(request.getRequestURI());
+			 
+			 return ResponseEntity.status(status).body(err);
+     }
+		
 }
--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -6,12 +6,15 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
+import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
 import com.devsuperior.dscatalog.services.exceptions.ResouceNotFoundException;
 
 @Service //ela registra a class como um componente que vai participar do sistema de injeção de dependencias automatizados do spring
@@ -59,6 +62,20 @@ public class CategoryService {
 		}
 		
 	}
+
+	// nao é necessario usar o @Transactional
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new ResouceNotFoundException("Id not found"+ id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		
 	
+	}
 	
+	}
 }
