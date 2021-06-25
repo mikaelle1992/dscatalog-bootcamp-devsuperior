e70280b3a19bf62ddf62bc402be7fcde676787fe
mikaelle1992 <mikaelle.rubiaps02@gmail.com>
2021-06-25 20:27:31 -0300

    feature:ProductRepository TestJUnit

--- backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.dao.EmptyResultDataAccessException;
 
 import com.devsuperior.dscatalog.entities.Product;
+import com.devsuperior.dscatalog.tests.Factory;
 
 @DataJpaTest
 public class ProductRepositoryTests {
@@ -19,17 +20,52 @@ public class ProductRepositoryTests {
 	@Autowired
 	private ProductRepository repository;
 	
-	private Long exintingId;
-	private Long nonExintingId;
+	private long exintingId;
+	private long nonExintingId;
+	private long countProduct;
 	
 	@BeforeEach
 	void setUp() throws Exception {
 		exintingId = 1L;
 		nonExintingId = 1000L;
+		countProduct = 25L;
+	}
+	
+	@Test
+	public void saveShoulPersisWithAutoincrementWhenIdsNull () {
+		Product product = Factory.createProduct();
+		product.setId(null);
+		product = repository.save(product);
+		
+		Assertions.assertNotNull(product.getId());
+		Assertions.assertEquals(product.getId(), countProduct+1);
+		
 	}
 	
 	@Test 
-	public void deleteShoulDeleteObjectWhenIdExists() {
+	public void retunEmptyShoulOptionalWhenIdDoesNotExists() {
+		
+		
+		repository.findById(nonExintingId);
+		
+		Optional<Product> result = repository.findById(nonExintingId);
+		Assertions.assertFalse(result.isPresent());
+		
+	}
+	
+	@Test 
+	public void retunNotEmptyShoulOptionalWhenIdExists() {
+		
+
+		repository.findById(exintingId);
+		
+		Optional<Product> result = repository.findById(exintingId);
+		Assertions.assertTrue(result.isPresent());
+		
+	}
+	
+	@Test 
+	public void deleteShoulDeleteObjectWhenIdDoesNotExists() {
 		
 
 		repository.deleteById(exintingId);
